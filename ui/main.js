cbfb47302f2bce9f953e6ce421b8c36acf74274a
Aravind <aravindrckumar@gmail.com>
2017-09-18 01:23:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,12 +2,26 @@ console.log('Loaded!');
 var img = document.getElementById('pic');
 
 var marginLeft = 0;
+var marginRight = 0;
 function moveRight() {
-    marginLeft += 10;
-    img.style.marginLeft = marginLeft + 'px';
+    if(hitRight !== true ) {
+        marginLeft += 10;
+        img.style.marginLeft = marginLeft + 'px';
+        if(marginLeft === 100 )
+            hitRight = true;
+    }
+    else if(hitLeft !== true) {
+        marginRight += 10;
+        img.style.marginRight = marginRight + 'px';
+        if(marginRight === 100 )
+            hitLeft = false;
+    }
 }
 
 img.onclick = function () {
     var interval = setInterval(moveRight, 50);
 };
 
+function animate() {
+    
+}
